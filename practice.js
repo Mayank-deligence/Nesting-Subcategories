070b615d98cb118dc8ab4054f83f5629df12ddb5
Mayanksomani04 <mayanksomani6@gmail.com>
2023-03-30 09:49:17 +0530

    again commit

--- practice.js
+++ practice.js
@@ -1,5 +1,93 @@
+
+function deletePanel(panelIndex, subcatIndex = null) {
+  if (subcatIndex !== null) {
+    // Delete the subcategory  
+    panels[panelIndex].subcategories.splice(subcatIndex, 1);
+    // console.log(panels[panelIndex].subcategories.splice(subcatIndex, 1))                  
+    // console.log(subcatIndex)
+    // Update the UI for the subcategories
+    const subCat = document.getElementById(`subCat-${panelIndex}`);
+    subCat.innerHTML = panels[panelIndex].subcategories.map((subcat, index) => `
+      <div class="accordion-item form-control">
+        <h2 class="accordion-header " id="panelsStayOpen-heading-${panelIndex}-${index}">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}-${index}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}-${index}">
+            ${subcat.name}  
+          </button>     
+          <div class="accordion-delete-btn">
+            <button class="btn btn-danger" type="button" onclick="deletePanel(${panelIndex},${index})">Delete</button>
+          </div> 
+        </h2>
+        <div id="panelsStayOpen-collapse-${panelIndex}-${index}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}-${index}">
+          <div class="accordion-body ">
+            <div class="Add input-group mb-3"> 
+              <label for="name1"></label>               
+              <input type="text" class="form-control" id="name1-${panelIndex}-${index}">
+              <button class="btn btn-primary" onclick="addSubcategory(${panelIndex})">Add</button>
+            </div>
+            <div id="subCat-${panelIndex}-${index}">        
+            </div>
+          </div>
+        </div>
+      </div>  
+    `).join("");
+  } else {
+    // Delete the panel  
+    panels.splice(panelIndex, 1);             
+    
+    // Update the UI for the panels
+    const output = document.getElementById("output");
+    output.innerHTML = panels.map((panel, panelIndex) => `
+      <div class="accordion-item form-control">
+        <h2 class="accordion-header " id="panelsStayOpen-heading-${panelIndex}">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}">
+            ${panel.header}
+            <div class="accordion-delete-btn">
+              <button class="btn btn-danger" type="button" onclick="deletePanel(${panelIndex})">Delete</button>
+            </div>
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapse-${panelIndex}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}">
+          <div class="accordion-body">
+            <div class="Add input-group mb-3">
+              <label for="name1"></label>
+              <input type="text" class="form-control" id="name1-${panelIndex}">
+              <button class="btn btn-primary" onclick="addSubcategory(${panelIndex})">Add</button>
+            </div>  
+            <div id="subCat-${panelIndex}">   
+            ${panel.subcategories ? (panel.subcategories.map(((subcat, index) => `
+            <div class="accordion-item form-control">
+            <h2 class="accordion-header" id="panelsStayOpen-heading-${panelIndex}-${index}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}-${index}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}-${index}">
+            ${subcat.name}
+            <div class="accordion-delete-btn">
+            <button class="btn btn-danger" type="button" onclick="deletePanel(${panelIndex},${index})">Delete</button>
+            </div>
+            </button>
+            </h2>
+            <div id="panelsStayOpen-collapse-${panelIndex}-${index}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}-${index}">
+            <div class="accordion-body ">
+            <div class="Add input-group mb-3">
+            <label for="name1"></label>
+            <input type="text" class="form-control" id="name1-${panelIndex}-${index}">
+            <button class="btn btn-primary" onclick="addSubcategory(${panelIndex})">Add</button>
+            </div>
+            <div id="subCat-${panelIndex}-${index}">
+            </div>
+            </div>
+            </div>
+            </div>
+            `)).join("")) : ""}          
+            </div>
+          </div>
+        </div>  
+      </div>
+    `).join("");
+  }
+}
+
 const panels = [];
-// console.log(typeof(panels))
+
+
 
 function addSubcategory(panelIndex) {
   const name1 = document.getElementById(`name1-${panelIndex}`).value;
@@ -16,13 +104,17 @@ function addSubcategory(panelIndex) {
       <h2 class="accordion-header " id="panelsStayOpen-heading-${panelIndex}-${index}">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}-${index}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}-${index}">
           ${subcat.name}
+          <div class="accordion-delete
+          -btn">
+          <button class="btn btn-danger" type="button" onclick="deletePanel(${panelIndex},${index})">Delete</button>
+          </div>
         </button>
       </h2>
       <div id="panelsStayOpen-collapse-${panelIndex}-${index}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}-${index}">
         <div class="accordion-body ">
           <div class="Add input-group mb-3"> 
             <label for="name1"></label>               
-            <input type="text" class="form-control" id="name1-${panelIndex}-${index}">
+            <input type="text" class="form-control" id="name2-${panelIndex}-${index}">
             <button class="btn btn-primary" onclick="addSubcategory(${panelIndex})">Add</button>
           </div>
           <div id="subCat-${panelIndex}-${index}">
@@ -43,14 +135,17 @@ function displayInput() {
   output.innerHTML = panels.map((panel, panelIndex) => `
       <div class="accordion-item form-control">
         <h2 class="accordion-header " id="panelsStayOpen-heading-${panelIndex}">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}">
-            ${panel.header}
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}">
+        ${panel.header}
+            <div class="accordion-delete-btn">
+            <button class="btn btn-danger" type="button" onclick="deletePanel(${panelIndex})">Delete</button>
+            </div>
           </button>
         </h2>
         <div id="panelsStayOpen-collapse-${panelIndex}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}">
           <div class="accordion-body">
             <div class="Add input-group mb-3">
-              <label for="name1"></label>
+              <label for="name1"></label>                
               <input type="text" class="form-control" id="name1-${panelIndex}">
               <button class="btn btn-primary" onclick="addSubcategory(${panelIndex})">Add</button>
             </div>  
@@ -60,6 +155,9 @@ function displayInput() {
              <h2 class="accordion-header" id="panelsStayOpen-heading-${panelIndex}-${index}">
              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}-${index}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}-${index}">
              ${subcat.name}
+             <div class="accordion-delete-btn">
+             <button class="btn btn-danger" type="button" onclick="deletePanel(${panelIndex},${index})">Delete</button>
+             </div>
              </button>
              </h2>
              <div id="panelsStayOpen-collapse-${panelIndex}-${index}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}-${index}">
@@ -83,13 +181,34 @@ function displayInput() {
   document.getElementById("name").value = "";
 }
 
-
-
-
-
-
-
-
-
-
-
+               // function subSubcategory(panelIndex, subcatIndex) {
+               //   const name2 = document.getElementById(`name2-${panelIndex}-${subcatIndex}`).value;
+               //   if (!panels[panelIndex].subcategories[subcatIndex].subsubcategories) {
+               //     panels[panelIndex].subcategories[subcatIndex].subsubcategories = [];
+               //   }
+               //   panels[panelIndex].subcategories[subcatIndex].subsubcategories.push({ name: name2 });
+               //   const subSubCat = document.getElementById(`subSubCat-${panelIndex}-${subcatIndex}`)
+               //   subSubCat.innerHTML = panels[panelIndex].subcategories[subcatIndex].subsubcategories.map((subsubcat, index) => `
+               //     <div class="accordion-item form-control">
+               //       <h2 class="accordion-header" id="panelsStayOpen-heading-${panelIndex}-${subcatIndex}-${index}">
+               //         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-${panelIndex}-${subcatIndex}-${index}" aria-expanded="false" aria-controls="panelsStayOpen-collapse-${panelIndex}-${subcatIndex}-${index}">
+               //           ${subsubcat.name}
+                         
+               //         </button>
+               //       </h2>
+               //       <div id="panelsStayOpen-collapse-${panelIndex}-${subcatIndex}-${index}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${panelIndex}-${subcatIndex}-${index}">
+               //         <div class="accordion-body">
+               //           <div class="Add input-group mb-3">
+               //             <label for="name2"></label>
+               //             <input type="text" class="form-control" id="name2-${panelIndex}-${subcatIndex}-${index}">
+               //             <button class="btn btn-primary" onclick="subSubcategory(${panelIndex}, ${subcatIndex})">Add</button>
+               //           </div>
+               //           <div id="subSubCat-${panelIndex}-${subcatIndex}-${index}">
+               //           </div>
+               //         </div>
+               //       </div>
+               
+               //     </div>
+               //   `).join("");
+               //   document.getElementById(`name2-${panelIndex}-${subcatIndex}`).value = "";
+               // }
